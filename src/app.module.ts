da30faff213fb67f19a1cdf642f8ca8ae3198ab4
Otavio Prada <otaviovinicius07@hotmail.com>
2025-10-16 22:21:31 -0300

    feat: auth and guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './modules/user/user.controller';
+import { AuthModule } from './modules/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './modules/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UsersModule,],
   controllers: [UserController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/modules/user/user.module.ts
@@ -0,0 +1,11 @@
+
+import { Module } from '@nestjs/common';
+import { UsersService } from './user.service';
+import { UserController } from './user.controller';
+
+@Module({
+    controllers: [UserController],
+  providers: [UsersService],
+  exports: [UsersService],
+})
+export class UsersModule {}
